b6bfe47ac9c09d8711bf4a9e3d5611524f8f0b57
testing-profile <skeenburg@gmail.com>
2021-02-17 20:48:39 -0600

    arebdny

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask
 import os
 from flask_cors import CORS, cross_origin
-import jerma face recog
-import getpicture from jerma face recog
+import jerma-face-recog
+import getpicture from jerma-face-recog
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
