b7df8e7ad3af4b1feda52af63a6cd35ee584a122
testing-profile <skeenburg@gmail.com>
2021-02-17 19:24:34 -0600

    hjgjkkj

--- server.py
+++ server.py
@@ -1,2 +1,11 @@
-from app import app
+from flask import Flask
+import os
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+@app.route('/', methods=['GET'])
+@cross_origin()
+def index():
+    return "Hello, World!"
